229a172303768f9fbd621bfc2db5f56732d77b91
Iuri <malinoski.iuri@gmail.com>
2020-09-01 14:29:00 -0300

    part 4 done - smart way to use urls; upgrade the use views

--- polls/views.py
+++ polls/views.py
@@ -6,22 +6,57 @@ from .models import Question
 
 
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #output = ', '.join([q.question_text for q in latest_question_list])
-    #return HttpResponse(output)
+    '''
+    return HttpResponse("Hello, world. You're at the polls index.")
+    '''
 
+    '''
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+    '''
+    
+    '''
+    # The logic happen here and return a basic response page.**
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+    ''' 
+
+    '''
+    # The logic happen here and the template are used properly
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    '''
+    
+    # The before, but more resumed
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
+    
+    '''
+    # Simple http page response. Just a text showed in the html
+    return HttpResponse("You're looking at question %s." % question_id)
+    ''' 
+
+    '''
+    # If no found element, an error page will displayed
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})  
+    return render(request, 'polls/detail.html', {'question': question})          
+    '''
+    
+    # The same as before, but shorter (the error message are build automaticaly, ex.: 'No Question matches the given query.')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
